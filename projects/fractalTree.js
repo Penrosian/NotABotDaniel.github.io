3b5808faea6e246be88abea7a19347595639534d
NotABotDaniel <dcgottshall@icloud.com>
2025-04-24 11:57:30 -0400

    adding javascript stuff to projects page

--- projects/fractalTree.js
+++ projects/fractalTree.js
@@ -1,32 +1,40 @@
 const canvas = document.getElementById("myCanvas");
 const ctx = canvas.getContext("2d");
 
-d = 90;
+let d = 90;
 
 function setD(dir){
+    console.log("setD");
     if (dir > 360){
         d = dir - 360;
-        setD(dir);
+        setD(d);
     } else if (dir < 0){
         d = dir + 360;
-        setD(dir);
+        setD(d);
     } else {
         d = dir;
     }
 }
 
 function branch(x, y, leng, dir) {
+    console.log("branch");
     setD(dir);
+    ctx.lineWidth = leng / 5;
     ctx.beginPath();
     ctx.moveTo(x, y);
     ctx.lineTo(x + leng * Math.cos(d * Math.PI / 180), y + leng * Math.sin(d * Math.PI / 180));
     ctx.stroke();
 
     if (leng > 2) {
-        branch(x + leng * Math.cos(d * Math.PI / 180), y + leng * Math.sin(d * Math.PI / 180), leng / 2, dir - 20);
-        branch(x + leng * Math.cos(d * Math.PI / 180), y + leng * Math.sin(d * Math.PI / 180), leng / 2, dir + 20);
+        console.log("firstBranch");
+        branch(x + leng * Math.cos(d * Math.PI / 180), y + leng * Math.sin(d * Math.PI / 180), (leng / 4) * 3, dir - 20);
+        console.log("secondBranch");
+        branch(x + leng * Math.cos(d * Math.PI / 180), y + leng * Math.sin(d * Math.PI / 180), (leng / 4) * 3, dir + 20);
     }
 }
 
-ctx.fillStyle='white';
-branch(999,400,64,0);
+ctx.fillStyle = "white";
+ctx.fillRect(0,0,1000,1000);
+ctx.fill();
+ctx.fillStyle = "black"
+branch(500,500,64,0);
